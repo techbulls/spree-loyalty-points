d790be3c2913ecc7c5faee1792c997273e26e312
kunal herkal <kunalrherkal@gmail.com>
2015-07-09 17:32:57 +0530

    Removed unnecessary logs.

--- app/controllers/spree/user_controller_decorator.rb
+++ app/controllers/spree/user_controller_decorator.rb
@@ -2,17 +2,13 @@ Spree::UserRegistrationsController.class_eval do
   after_filter :credit_loyalty_points_to_referral_user, :only => :create
   
   def credit_loyalty_points_to_referral_user
-    puts "In credit_loyalty_points_to_referral_user"
   	referred_records = Spree::ReferredRecord.where(:user_id => @user.id)
   	referred_records.each do |referred_record|
-      puts "referred_record found!"
   		if !referred_record.loyalty_points_awarded
 	  		referral_user_id = referred_record.referral_id
 	  		referral_user = Spree::User.find_by(:id => referral_user_id)
-        puts "loyalty_points_balance: " + referral_user.loyalty_points_balance.to_s
 	  		referral_user.loyalty_points_balance += Spree::Config.loyalty_points_per_referred_user
         referral_user.save!
-        puts "loyalty_points_balance: " + referral_user.loyalty_points_balance.to_s
 	  		referred_record.loyalty_points_awarded = true
 	  		referred_record.save!
   		end
